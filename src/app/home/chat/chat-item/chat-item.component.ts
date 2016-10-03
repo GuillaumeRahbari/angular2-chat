64e06c990778477571d3938eb06ada4bf67e5f52
Rahbari Guillaume <guillaume.rahbari@gmail.com>
2016-10-03 12:56:08 +0200

    chat item component

--- src/app/home/chat/chat-item/chat-item.component.ts
+++ src/app/home/chat/chat-item/chat-item.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input} from '@angular/core';
+import {User} from "../../../models/user";
 
 @Component({
   selector: 'app-chat-item',
   templateUrl: './chat-item.component.html',
   styleUrls: ['./chat-item.component.sass']
 })
-export class ChatItemComponent implements OnInit {
+export class ChatItemComponent{
+
+  @Input() user: User;
+
+  userMsg: string;
 
   constructor() { }
 
-  ngOnInit() {
+  /**
+   * This function is triggered when we click on the form's submit button.
+   */
+  onSubmit() {
   }
 
 }
